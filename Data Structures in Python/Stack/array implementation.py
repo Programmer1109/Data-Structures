919c3849d11ba1d52d2440cf373a07cf85cfca7d
Anthony D'souza <jordananthony786@gmail.com>
2022-03-24 18:20:32 +0530

    Implementation of Stack DS using array

--- Data Structures in Python/Stack/array implementation.py
+++ Data Structures in Python/Stack/array implementation.py
@@ -1,21 +1,27 @@
+# Implentation of Stack Data Structure using array.
 class Stack:
     def __init__(self):
         self.list = []
 
-    def push(self, value):
+    def push(self, value, size):
         global top
-        top += 1
-        self.list.append(value)
-        print(self.list)
-        print(self.peek())
+        if top == size:
+            print("Overflow Error:- cannot push in an already full list.\n")
+        else:
+            top = top + 1
+            self.list.append(value)
+            print(self.list)
+            print(self.peek())
 
     def pop(self):
         global top
         if top == -1:
-            print("cannot pop from an empty stack")
+            print("Underflow Error:- cannot pop from an empty stack\n")
         else:
-            top -= 1
+            top = top - 1
             del self.list[-1]
+            print(self.list)
+            print(self.peek())
 
     def peek(self):
         if top != -1:
@@ -24,13 +30,22 @@ class Stack:
             return -1
 
 
-stack = Stack()
+print("********************\t\tArray Implementation of Stacks\t\t********************\n\n")
+stackList = Stack()
 top = -1
-roster = [12, 34, 43, 71, 65, 92]
-for value in roster:
-    stack.push(value)
-for value in roster:
-    stack.pop()
-    print(stack.list)
-    print(stack.peek())
-stack.pop()
+total = int(input(print("Enter no. of elements:- ")))
+while(True):
+    choice = input(print("Choose operation to be performed:-\n\t1. Push operation\n\t2. Pop operation\n\t3. Peek operation\n\t4. Quit\nEnter your choice:- "))
+    if choice == '1':
+        elmentValue = int(input(print("Enter element to stack")))
+        stackList.push(elmentValue, total)
+    elif choice == '2':
+        stackList.pop()
+    elif choice == '3':
+        val = stackList.peek()
+        print(f"Peek = {val}")
+    elif choice == '4':
+        break
+    else:
+        print("Error:- Invalid choice of operation!!!")
+print("\n\n********************\t\tEND of Program\t\t********************")
