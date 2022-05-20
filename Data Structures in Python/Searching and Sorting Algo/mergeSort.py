0d1609ee2e375c81389a00452fed087d16249bd0
Anthony D'souza <jordananthony786@gmail.com>
2022-05-20 09:14:49 +0530

    Update mergeSort.py

--- Data Structures in Python/Searching and Sorting Algo/mergeSort.py
+++ Data Structures in Python/Searching and Sorting Algo/mergeSort.py
@@ -1,9 +1,4 @@
-# Python program for implementation of MergeSort
- 
-# Merges two subarrays of arr[].
-# First subarray is arr[l..m]
-# Second subarray is arr[m+1..r]
-
+                  # Python program for implementation of MergeSort
 
 def Merge(array, left, mid, right):
     # print("\nIn Merge Function:- \n")
@@ -49,52 +44,6 @@ def Merge(array, left, mid, right):
         arrayIndex += 1
         rightIndex += 1
     # print(array)
-
-
-"""
-def Merge(arr, l, m, r):
-    n1 = m - l + 1
-    n2 = r - m
- 
-    # create temp arrays
-    L = [0] * (n1)
-    R = [0] * (n2)
- 
-    # Copy data to temp arrays L[] and R[]
-    for i in range(0, n1):
-        L[i] = arr[l + i]
- 
-    for j in range(0, n2):
-        R[j] = arr[m + 1 + j]
- 
-    # Merge the temp arrays back into arr[l..r]
-    i = 0     # Initial index of first subarray
-    j = 0     # Initial index of second subarray
-    k = l     # Initial index of merged subarray
- 
-    while i < n1 and j < n2:
-        if L[i] <= R[j]:
-            arr[k] = L[i]
-            i += 1
-        else:
-            arr[k] = R[j]
-            j += 1
-        k += 1
- 
-    # Copy the remaining elements of L[], if there
-    # are any
-    while i < n1:
-        arr[k] = L[i]
-        i += 1
-        k += 1
- 
-    # Copy the remaining elements of R[], if there
-    # are any
-    while j < n2:
-        arr[k] = R[j]
-        j += 1
-        k += 1
-"""
 
 
 def mergeSort(array, low, high):
