be35717c84d3ea582e34107b60267d3b6d07a32a
Anthony D'souza <jordananthony786@gmail.com>
2022-11-10 17:44:32 +0530

    Update priorityQueue.py

--- Data Structures in Python/Queues/priorityQueue.py
+++ Data Structures in Python/Queues/priorityQueue.py
@@ -22,47 +22,61 @@ class priorityQueue:
             print("Error: Queue is empty.")
         else:
             ptr = self.start
-            print("Process\tProcess ID\tPriority\tNext Process")
+            print("Process\t\t\tProcess ID\t\t\tPriority\t\t\tNext Process")
             while ptr.next is not None:
                 # print("Hello...")
                 print(f"{ptr}   {ptr.processID}   {ptr.priority}   {ptr.next}")
                 ptr = ptr.next
             print(f"{ptr}   {ptr.processID}   {ptr.priority}   {ptr.next}")
 
+
+    def check_processID(self, value):
+        pointer = self.start
+        while pointer is not None:
+            if pointer.processID == value:
+                return True
+            else:
+                pointer = pointer.next
+                continue
+        return False
+
     def enqueue(self):
         newNode = Process()
-        print("in enqueue...")
-        print("start Node-> before = ", self.start)
-        if self.start is None:
-            print("Inserting in empty queue...")
-            self.start = newNode
-            print("start Node-> after =", self.start)
+        # print("in enqueue...")
+        # print("start Node-> before = ", self.start)
+        if self.check_processID(newNode.processID):
+            print("Error:- Node is already present in the Queue!!!")
         else:
-            print("Inserting Node...")
-            if self.start is None or newNode.priority < self.start.priority:
-                print("Inserting New Node at the Beginning...")
-                print(f"Start Node:- before -> {self.start}")    
-                newNode.next = self.start
+            if self.start is None:
+                # print("Inserting in empty queue...")
                 self.start = newNode
-                print(f"Start Node:- after -> {self.start}")
+                # print("start Node-> after =", self.start)
             else:
-                Ptr = self.start
-                while (Ptr.next != None and Ptr.next.priority <= newNode.priority):
-                    Ptr = Ptr.next
-                newNode.next = Ptr.next
-                Ptr.next = newNode
+                # print("Inserting Node...")
+                if self.start is None or newNode.priority < self.start.priority:
+                    # print("Inserting New Node at the Beginning...")
+                    # print(f"Start Node:- before -> {self.start}")    
+                    newNode.next = self.start
+                    self.start = newNode
+                    # print(f"Start Node:- after -> {self.start}")
+                else:
+                    Ptr = self.start
+                    while (Ptr.next != None and Ptr.next.priority <= newNode.priority):
+                        Ptr = Ptr.next
+                    newNode.next = Ptr.next
+                    Ptr.next = newNode
 
     def dequeue(self):
         if self.start is None:
             print("Underflow Error: The Queue is empty. Hence, pop operation can't be performed.")
         elif self.start.next is None:
-            print(f"Start Node: before -> {self.start}   {self.start.processID}")
+            # print(f"Start Node: before -> {self.start}   {self.start.processID}")
             self.start = None
-            print(f"Start Node: after -> {self.start}  ")
+            # print(f"Start Node: after -> {self.start}  ")
         else:
-            print(f"Start Node: before -> {self.start}   {self.start.processID}")
+            # print(f"Start Node: before -> {self.start}   {self.start.processID}")
             self.start = self.start.next
-            print(f"Start Node: after -> {self.start}   {self.start.processID}")
+            # print(f"Start Node: after -> {self.start}   {self.start.processID}")
 
 
 
@@ -86,3 +100,4 @@ while True:
     else:
         print("Inavild Choice")
 print("\n*************************\t     END OF PROGRAM     \t*************************\n")
+                
